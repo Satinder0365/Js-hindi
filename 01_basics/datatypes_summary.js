3efb75d88b2f59ccd3c47af6f3216e02d01fc20e
Satinder0365 <123872537+Satinder0365@users.noreply.github.com>
2023-10-11 16:46:30 +0000

    summay

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,26 @@
+// JavaScript is a dynmically typed language 
+
+// Pritive Datatypes
+// 7 types : String, Number, Null, Undefined, Boolean, Symbol (used to make uniqe value),BigInt
+// const score=100
+// const scorevalue=19.56
+// const isLoggedIn= false
+// const outside =null
+// let userEmail;
+// const id=Symbol('123')
+// const anotherId=Symbol('123')
+
+// console.log(id==anotherId);
+
+
+// Non primitive, Reference Types
+
+// Array, Objects, Functions
+
+const heros=["shaktiman" , "Ram", "Hanuman"];
+console.log(heros)
+let myObj={
+    name:"Satinder",
+    age:22,
+}
+
