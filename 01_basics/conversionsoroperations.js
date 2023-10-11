ae8ac0fc35844d547b9599f1afae03a25491a6de
Satinder0365 <123872537+Satinder0365@users.noreply.github.com>
2023-10-11 11:05:44 +0000

    conversion of datatypes

---
+++ 01_basics/conversionsoroperations.js
@@ -0,0 +1,33 @@
+/*let score = "33abc"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log (typeof valueInNumber);
+console.log(valueInNumber);*/
+
+
+///let age= "45avc"
+//console.log(typeof age)
+
+//let Innumber =Number(age)
+//console.log(typeof Innumber)
+//console.log(Innumber);
+
+//"33" => easily convert in 33
+//"33abc"  => NaN  Not a Number
+//true => 1; false => 0;
+
+////et isLoggedIn=0
+
+////let booleanIsLogged = Boolean(isLoggedIn)
+//console.log(booleanIsLogged);
+
+//1 convert in true ,and 0 convert in 1
+
+let someNumber = 33
+let someNumberIs = String(someNumber)
+console.log(someNumberIs)
+console.log(typeof someNumberIs)
+
+//convert someNumber into String 
+
