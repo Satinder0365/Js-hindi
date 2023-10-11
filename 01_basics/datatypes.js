54f381fc5e770985a2223096f245777e7955e522
Satinder0365 <123872537+Satinder0365@users.noreply.github.com>
2023-10-11 04:45:57 +0000

    confic

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"  //  treate al code JS code as newer version
+
+//alert("hello") we raee using not JS not browser
+let name="Satinder"
+let age=22
+console.log(name)
+console.log(age)
+//let LoggedIn =false
+//datatypes
+//number => 2 to the power of 53
+//bigint 
+// String => ""
+//boolean= true/flase
+//null=> standlone value
+//undefined=>value is not assined yet
+//symbol =>uniqe
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined)//undefined
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,2 +1,10 @@
 const accountId="243343"
-let accontEamil="hitet@gmail.com"
+let accountEamil="hitet@gmail.com"
+var accountPassword ="1234"
+accountCity ="himachal"
+
+console.log(accountId);
+
+/* Prefer not use var
+beacuse of issue in block scope and functional scope*/
+console.table([accountId, accountPassword,accountEamil, accountCity])
