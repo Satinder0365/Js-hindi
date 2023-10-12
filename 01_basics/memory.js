bc145f59cd8f44abd5f133333f571735dd3e0d10
Satinder0365 <123872537+Satinder0365@users.noreply.github.com>
2023-10-12 16:23:45 +0000

    memories

---
+++ 01_basics/memory.js
@@ -0,0 +1,14 @@
+// Two types memory
+
+// 1. Stack 2. Heap
+
+//Stack memory used in (Primitive Dataypes)
+// Heap memory used in (Non Primitive datatypes)
+
+//Example:
+ let myYoutubeName="SatinderSingh"
+
+ let anotherName= myYoutubeName
+ anotherName="Chaisutabar"
+console.log(myYoutubeName)
+ console.log(anotherName)
