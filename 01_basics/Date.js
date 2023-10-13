3c9e256362110c0e918f28d0005a6ab6331db842
Satinder0365 <123872537+Satinder0365@users.noreply.github.com>
2023-10-13 16:54:28 +0000

    dates

---
+++ 01_basics/Date.js
@@ -0,0 +1,19 @@
+//Dates
+
+ let myDate= new  Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleTimeString());
+
+//console.log(typeof myDate);
+
+ let mycreatedDate = new  Date(2023, 10, 23, 5, 4)
+ //console.log(mycreatedDate.toLocaleString())
+
+//  let myTImeStamps= Date.now()
+//  console.log(myTImeStamps)
+
+ let newDate = new Date()
+ console.log(newDate.getDate())
