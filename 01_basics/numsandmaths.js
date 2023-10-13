722278d003fcc24492f060acd79e132d66baa237
Satinder0365 <123872537+Satinder0365@users.noreply.github.com>
2023-10-13 16:23:29 +0000

    Math and Number

--- 01_basics/numsandmaths.js
+++ 01_basics/numsandmaths.js
@@ -1,3 +1,21 @@
-const score=409;
+// const score=409;
 
-console.log(score.toString());
+// console.log(score.toString());
+
+
+// const hundreds=100000
+// console.log(hundreds.toLocaleString('en-In'))
+
+
+/***       Maths           */
+
+console.log(Math)
+
+//console.log(Math.abs(-4));
+//console.log(Math.floor(4.7));
+
+console.log(Math.random()*10+1)
+
+const min=10
+const max= 20
+console.log(Math.floor(Math.random()*( max - min + 1))+min)
