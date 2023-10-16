0cd61c7fdf4219179fe0f4793f307f1771fd0b5f
Satinder0365 <123872537+Satinder0365@users.noreply.github.com>
2023-10-16 16:23:24 +0000

    vv

--- 01_basics/02_basics/arrays.js
+++ 01_basics/02_basics/arrays.js
@@ -7,6 +7,17 @@ const myarray= [3,2,4,5,6]
 //Array methods
 // myarray.push(8)
 // myarray.pop()
-myarray.unshift(9)
-myarray.shift()
-console.log(myarray)
+//myarray.unshift(9)
+//myarray.shift()
+
+// console.log(myarray.indexOf(9));
+// console.log(myarray)
+
+//slice, splice
+console.log("A ".myarray);
+const myn1=myarray.slice(1, 3)
+console.log(myn1)
+console.log("B ", myarray);
+
+const myn2=myarray.splice(1, 3)
+console.log(myn2)
